ee915b7a26b70011cc046998f0bc173f40d457a7
Emilo067 <gasanovemil467@gmail.com>
2024-09-18 11:02:56 +0300

    auth pages

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,8 @@
+import * as z from 'zod'
+
+export const SignupValidation = z.object({
+    name: z.string().min(2, {message: 'Слишком короткий'}),
+    username: z.string().min(2, {message: 'Слишком короткий'}),
+    email: z.string().email(),
+    password: z.string().min(8, {message: 'Пароль должен содержать минимум 8 символов'})
+})
