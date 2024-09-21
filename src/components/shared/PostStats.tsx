931fd16bf910958ebe15c3fbc29cde850c64323e
Emilo067 <gasanovemil467@gmail.com>
2024-09-21 15:38:42 +0300

    add page post details

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -10,12 +10,12 @@ import {checkIsLiked} from "@/lib/utils.ts";
 import Loader from "@/components/shared/Loader.tsx";
 
 type PostStatsProps = {
-    post: Models.Document,
+    post?: Models.Document,
     userId: string,
 }
 
 const PostStats = ({post, userId}: PostStatsProps) => {
-    const likesList = post.likes.map((user: Models.Document) => user.$id);
+    const likesList = post?.likes.map((user: Models.Document) => user.$id);
 
     const [likes, setLikes] = useState(likesList);
     const [isSaved, setIsSaved] = useState(false);
@@ -26,7 +26,7 @@ const PostStats = ({post, userId}: PostStatsProps) => {
 
     const {data: currentUser} = useGetCurrentUser();
 
-    const savedPostRecord = currentUser?.save.find((record: Models.Document) => record.post.$id === post.$id);
+    const savedPostRecord = currentUser?.save.find((record: Models.Document) => record.post.$id === post?.$id);
 
     // { saved: true } => !savedPostedRecord => !false => true
     // 'test' => !'test' => !false => true
@@ -48,7 +48,7 @@ const PostStats = ({post, userId}: PostStatsProps) => {
         }
 
         setLikes(newLikes);
-        likePost({postId: post.$id, likesArray: newLikes});
+        likePost({postId: post?.$id || '', likesArray: newLikes});
     }
 
     const handleSavePost = (e: MouseEvent) => {
@@ -58,7 +58,7 @@ const PostStats = ({post, userId}: PostStatsProps) => {
             setIsSaved(false);
             deleteSavedPost(savedPostRecord.$id);
         } else {
-            savePost({postId: post.$id, userId: userId});
+            savePost({postId: post?.$id || '', userId: userId});
             setIsSaved(true);
         }
     }
